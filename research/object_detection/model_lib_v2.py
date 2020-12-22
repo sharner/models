3f7fe278b842b419e40534839a536fb5644ad347
Forest Rouse <forest@layerjot.com>
2020-12-22 10:11:48 -0800

    Matched checkpoint load in https://gilberttanner.com/blog/object-detection-with-tensorflow-2

--- research/object_detection/model_lib_v2.py
+++ research/object_detection/model_lib_v2.py
@@ -374,12 +374,10 @@ def load_fine_tune_checkpoint(
       fine_tune_checkpoint_type=checkpoint_type)
   validate_tf_v2_checkpoint_restore_map(restore_from_objects_dict)
   # Change from https://github.com/tensorflow/models/issues/8399
-  model.load_weights(checkpoint_path).assert_existing_objects_matched()
+  # model.load_weights(checkpoint_path).assert_existing_objects_matched()
   # ckpt = tf.train.Checkpoint(**restore_from_objects_dict)
-  # ckpt = tf.compat.v2.train.Checkpoint(**restore_from_objects_dict)
-  # ckpt.restore(checkpoint_path).assert_existing_objects_matched()
-  # model.load_weights(ckpt).assert_existing_objects_matched()
-
+  ckpt = tf.compat.v2.train.Checkpoint(**restore_from_objects_dict)
+  ckpt.restore(checkpoint_path).expect_partial()
 
 def get_filepath(strategy, filepath):
   """Get appropriate filepath for worker.
